7f7b0308981202c015aae046d77e1b43d29bffde
layton <laytonmatheka@gmail.com>
2023-12-05 14:02:55 +0300

    email

--- app.py
+++ app.py
@@ -10,8 +10,8 @@ app.secret_key='futfygkbkjbkbdlkeougxx'
 
 app.config['MAIL_SERVER']='smtp.gmail.com'
 app.config['MAIL_PORT']=465
-app.config['MAIL_USERNAME']='laytonmatheka7@gmail.com'
-app.config['MAIL_PASSWORD']='qamfnggyldkpbhje'
+app.config['MAIL_USERNAME']='kabu.cs.hub@gmail.com'
+app.config['MAIL_PASSWORD']='vhdzrcqhzehtqufd'
 app.config['MAIL_USE_TLS']=False
 app.config['MAIL_USE_SSL']=True
 mail=Mail(app)
